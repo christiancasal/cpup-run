02bb8e70e0ae9e09669b40087b20c2c6436aafcd
ccasal <casal.christian@gmail.com>
2018-04-21 11:54:01 -0400

    more setup

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,8 +5,10 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { BlogPostsComponent } from './blog-posts/blog-posts.component';
 
 const routes: Routes = [
+	{ path: '', component: AboutMeComponent },
 	{ path: 'about', component: AboutMeComponent },
 	{ path: 'posts', component: BlogPostsComponent },
+	{ path: '**', redirectTo: 'posts' },
 ];
 
 @NgModule({
